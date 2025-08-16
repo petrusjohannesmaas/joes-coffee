dc47f9ceba852511ce867fb72529bf44c6375cad
Petrus Johannes Maas <petrusjohannesmaas@oultook.com>
2025-08-16 21:06:00 +0200

    SImplified design and routing

--- server.js
+++ server.js
@@ -13,38 +13,13 @@ app.get('/', (req, res) => {
   });
 });
 
-app.get('/about', (req, res) => {
-  res.render('about', {
-    pageTitle: 'About',
-    pageHeading: 'About',
-    bgImage: '/images/scooter.png'
-  });
-});
-
-app.get('/shop', (req, res) => {
-  res.render('shop', {
-    pageTitle: 'Shop',
-    pageHeading: 'Shop our fresh roasted coffees.',
-    bgImage: '/images/shop-banner.png'
-  });
-});
-
-app.get('/locations', (req, res) => {
-  res.render('locations', {
-    pageTitle: 'Locations',
-    pageHeading: 'Locations',
-    bgImage: '/images/locations-banner.png'
-  });
-});
-
 app.get('/wholesale', (req, res) => {
   res.render('wholesale', {
     pageTitle: 'Wholesale',
-    pageHeading: 'Wholesale',
-    bgImage: '/images/wholesale-banner.jpg'
+    pageHeading: 'Order in bulk',
+    bgImage: '/images/order-banner.png'
   });
 });
-
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
